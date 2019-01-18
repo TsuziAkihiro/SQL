40228628d25abefe9e8fe1a1877f37046f56d78e
辻昌紘 <tsujiakihiro@tsujimayuminoMacBook-puro.local>
2019-01-18 10:21:44 +0900

    Q9 SQL

--- Q9.sql
+++ Q9.sql
@@ -1,7 +1,7 @@
 SELECT
   item_category.category_name,
-  item_price
+  SUM(item_price) AS total_price
 FROM item_category INNER JOIN item
 ON item.category_id = item_category.category_id
-GROUP BY category_name
-ORDER BY item_price DESC;
+GROUP BY item.category_id
+ORDER BY total_price DESC;
